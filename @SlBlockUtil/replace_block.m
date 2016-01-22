bab04ddf5434162015d8e1cf895bcaaa2322623d
Aaron Cook <acook@allegromicro.com>
2016-01-22 12:56:09 -0500

    Repoen system after replacement is made

--- @SlBlockUtil/replace_block.m
+++ @SlBlockUtil/replace_block.m
@@ -3,7 +3,9 @@ function replace_block(obj,src_sys)
 % SRC_SYS - The name of the subsystem to copy from
 
 delete_block(obj.sys);
+open_system(obj.parent); % once the system is deleted, the diagram is closed, open the parent
 add_block(src_sys, obj.sys, 'CopyOption', 'duplicate', 'Position',obj.pos)
+
 
 end
 
